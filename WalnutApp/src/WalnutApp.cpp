dc8d50589b658578ab5735a89b8891cea27df70c
Window5000 <84613801+Window5000@users.noreply.github.com>
2022-05-27 14:16:49 +0200

    Update WalnutApp.cpp

--- WalnutApp/src/WalnutApp.cpp
+++ WalnutApp/src/WalnutApp.cpp
@@ -6,33 +6,39 @@
 class ExampleLayer : public Walnut::Layer
 {
 public:
+	virtual void OnDetach() override {
+		Squirrel::writeFile();
+	}
+
 	virtual void OnUIRender() override
 	{
-		ImGui::Begin("Hello");
-		ImGui::Button("Button");
-		ImGui::End();
-
-		ImGui::ShowDemoWindow();
+		Squirrel::Start();
+		
+		if(ImGui::BeginTabItem("Test1")) {
+			Squirrel::BuyButton("Example1", 5, "An example", 10.0f, SquirrelConf::ItemList::Example1, SquirrelConf::Type::Test1);
+		}
+		
+		if(ImGui::BeginTabItem("Test2")) {
+			Squirrel::BuyButton("Example2", 10, "Another example", 15.0f, SquirrelConf::ItemList::Example2, SquirrelConf::Type::Test2);
+		}
+		
+		Squirrel::End();
+		//ImGui::ShowDemoWindow();
 	}
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 {
+	Squirrel::addItems();
+	Squirrel::loadFile();
+	
 	Walnut::ApplicationSpecification spec;
-	spec.Name = "Walnut Example";
+	spec.Name = "Squirrel Example";
 
 	Walnut::Application* app = new Walnut::Application(spec);
 	app->PushLayer<ExampleLayer>();
-	app->SetMenubarCallback([app]()
-	{
-		if (ImGui::BeginMenu("File"))
-		{
-			if (ImGui::MenuItem("Exit"))
-			{
-				app->Close();
-			}
-			ImGui::EndMenu();
-		}
-	});
+	
+	Squirrel::MakeMenu();
+	
 	return app;
 }
